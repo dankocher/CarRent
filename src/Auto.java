9af3f955343fcfacfd3e3047495393e38a9ada57
snezhok-p <43536160+snezhok-p@users.noreply.github.com>
2019-10-21 17:56:53 +0300

    for rent Cars

---
+++ src/Auto.java
@@ -0,0 +1,47 @@
+public class Auto {
+    String color;
+    String mark;
+    String model;
+    String number;
+    int year;
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
